4fe8739d99e88ad62dcac46776ce076bf185fa0b
Md Irfan <rn555666777@gmail.com>
2025-12-06 14:48:58 +0530

    implementing crud functionality for Comments

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import Profile from "../models/profile.model.js";
 import Post from "../models/posts.model.js";
+import Comment from "../models/comments.model.js";
 import bcrypt from "bcrypt";
 
 //* =============== Checking Server is running or not! =============== *//
@@ -19,7 +20,7 @@ export const createPost = async (req, res) => {
     const post = new Post({
       userId: user._id,
       body: req.body.body,
-      likes: req.body.likes,
+      likes: req.body.likes || 0,
       media: req.file !== undefined ? req.file.filename : "",
       fileType: req.file !== undefined ? req.file.mimetype.split("/")[1] : "",
     });
@@ -62,10 +63,100 @@ export const deletePost = async (req, res) => {
       return res.status(400).json({ message: "Unauthorize!" });
     }
 
-    await post.deletePost({ _id: post_id });
+    await Post.deleteOne({ _id: post_id });
+    await Comment.deleteMany({ postId: post_id });
+
     return res.json({ message: "Post Deleted!" });
   } catch (err) {
     console.error("Error While Deleting Post! ", err.message);
+    return res.status(500).json({ message: "Server Error!" });
+  }
+};
+
+//* =============== Comment Posts =============== *//
+export const commentPost = async (req, res) => {
+  const { token, post_id, commentBody } = req.body;
+
+  try {
+    const user = await User.findOne({ token: token }).select("_id");
+    if (!user) return res.status(404).json({ message: "User not found!" });
+
+    const post = await Post.findOne({ _id: post_id });
+    if (!post) return res.status(404).json({ message: "Post not found!" });
+
+    const comment = new Comment({
+      userId: user._id,
+      postId: post_id,
+      body: commentBody,
+    });
+    await comment.save();
+
+    return res.status(200).json({ message: "Comment Added!" });
+  } catch (err) {
+    console.error("Error While Comment Post! ", err.message);
+    return res.status(500).json({ message: "Server Error!" });
+  }
+};
+
+//* =============== Get Comment Posts =============== *//
+export const get_comment_by_post = async (req, res) => {
+  const { post_id } = req.body;
+
+  try {
+    const post = await Post.findOne({ _id: post_id });
+    if (!post) return res.status(404).json({ message: "Post not found!" });
+
+    const comments = await Comment.find({ postId: post_id }).populate(
+      "userId",
+      "name username email profilePicture"
+    );
+
+    return res.json({ comments });
+  } catch (err) {
+    console.error("Error While Comment Post! ", err.message);
+    return res.status(500).json({ message: "Server Error!" });
+  }
+};
+
+//* =============== Delete Comment Posts =============== *//
+export const deleteCommentOfUser = async (req, res) => {
+  const { token, comment_id } = req.body;
+
+  try {
+    const user = await User.findOne({ token: token }).select("_id");
+    if (!user) return res.status(404).json({ message: "User not found!" });
+
+    const comment = await Comment.findOne({ _id: comment_id });
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found!" });
+    }
+
+    if (comment.userId.toString() !== user._id.toString()) {
+      return res.status(400).json({ message: "Unauthorize!" });
+    }
+
+    await Comment.deleteOne({ _id: comment_id });
+    return res.json({ message: "Comment Deleted!" });
+  } catch (err) {
+    console.error("Error While Comment Post! ", err.message);
+    return res.status(500).json({ message: "Server Error!" });
+  }
+};
+
+//* =============== Increament Likes =============== *//
+export const increament_Likes = async (req, res) => {
+  const { post_id } = req.body;
+
+  try {
+    const post = await Post.findOne({ _id: post_id });
+    if (!post) return res.status(404).json({ message: "Post not found!" });
+
+    post.likes += 1;
+    await post.save();
+
+    return res.json({ message: "Likes increased!" });
+  } catch (err) {
+    console.error("Error While Increament Likes! ", err.message);
     return res.status(500).json({ message: "Server Error!" });
   }
 };
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import {
   activeCheck,
+  commentPost,
   createPost,
+  deleteCommentOfUser,
   deletePost,
+  get_comment_by_post,
   getAllPosts,
+  increament_Likes,
 } from "../controllers/posts.controller.js";
 import multer, { diskStorage } from "multer";
 
@@ -20,8 +24,16 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.get("/", activeCheck);
+
+// Post routes
 router.post("/create_post", upload.single("media"), createPost);
 router.get("/posts", getAllPosts);
 router.delete("/delete_post", deletePost);
+
+// Comment Routes
+router.post("/comment", commentPost);
+router.get("/get_comments", get_comment_by_post);
+router.delete("/delete_comment", deleteCommentOfUser);
+router.post("/increment_post_likes", increament_Likes);
 
 export default router;

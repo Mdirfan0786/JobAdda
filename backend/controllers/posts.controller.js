eeef798f2c0480250c72693d440c6926c4bc9420
Md Irfan <rn555666777@gmail.com>
2025-12-06 09:58:36 +0530

    Delete Route Implemented!

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -3,11 +3,12 @@ import Profile from "../models/profile.model.js";
 import Post from "../models/posts.model.js";
 import bcrypt from "bcrypt";
 
-// Checking Server is running or not!
+//* =============== Checking Server is running or not! =============== *//
 export const activeCheck = async (req, res) => {
   return res.status(200).json({ message: "RUNNING" });
 };
 
+//* =============== Creating Posts =============== *//
 export const createPost = async (req, res) => {
   const { token } = req.body;
 
@@ -28,5 +29,43 @@ export const createPost = async (req, res) => {
   } catch (err) {
     console.error("Error While Creating Post");
     return res.json({ message: "Server Error!" });
+  }
+};
+
+//* =============== Getting All Posts =============== *//
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate(
+      "userId",
+      "name username email profilePicture"
+    );
+
+    return res.json({ posts });
+  } catch (err) {
+    console.error("Error While Getting All Posts! ", err.message);
+    return res.status(500).json({ message: "Server Error!" });
+  }
+};
+
+//* =============== Delete Posts =============== *//
+export const deletePost = async (req, res) => {
+  const { token, post_id } = req.body;
+
+  try {
+    const user = await User.findOne({ token: token }).select("_id");
+    if (!user) return res.status(404).json({ message: "User not found!" });
+
+    const post = await Post.findOne({ _id: post_id });
+    if (!post) return res.status(404).json({ message: "Post not found!" });
+
+    if (post.userId.toString() !== user._id.toString()) {
+      return res.status(400).json({ message: "Unauthorize!" });
+    }
+
+    await post.deletePost({ _id: post_id });
+    return res.json({ message: "Post Deleted!" });
+  } catch (err) {
+    console.error("Error While Deleting Post! ", err.message);
+    return res.status(500).json({ message: "Server Error!" });
   }
 };
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { activeCheck, createPost } from "../controllers/posts.controller.js";
+import {
+  activeCheck,
+  createPost,
+  deletePost,
+  getAllPosts,
+} from "../controllers/posts.controller.js";
 import multer, { diskStorage } from "multer";
 
 const router = Router();
@@ -16,5 +21,7 @@ const upload = multer({ storage: storage });
 
 router.get("/", activeCheck);
 router.post("/create_post", upload.single("media"), createPost);
+router.get("/posts", getAllPosts);
+router.delete("/delete_post", deletePost);
 
 export default router;

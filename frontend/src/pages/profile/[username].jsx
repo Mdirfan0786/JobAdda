02e2a8ebb65728c75af5ca4da3149d3f4fb756ef
Md Irfan <rn555666777@gmail.com>
2026-01-29 21:50:29 +0530

    feat: add profile pic upadation

--- frontend/src/pages/profile/[username].jsx
+++ frontend/src/pages/profile/[username].jsx
@@ -136,6 +136,27 @@ export default function ProfileComponent() {
     }
   };
 
+  const handleProfilePicUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    const formData = new FormData();
+    formData.append("profile_picture", file);
+    formData.append("token", token);
+
+    try {
+      await clientServer.post("/upload_profile_picture", formData);
+
+      dispatch(getAboutUser());
+    } catch (err) {
+      console.error("Background upload failed", err);
+      alert("Background upload failed");
+    }
+  };
+
   const userProfile = authState.user;
 
   // Own prifile
@@ -206,6 +227,37 @@ export default function ProfileComponent() {
                     )}
 
                     <div className={styles.profile_pic}>
+                      <div className={styles.profilePicUpdate}>
+                        <div
+                          onClick={() =>
+                            document.getElementById("ProfilePicInput").click()
+                          }
+                          className={styles.profilePic_edit_btn}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                            />
+                          </svg>
+                        </div>
+
+                        <input
+                          type="file"
+                          id="ProfilePicInput"
+                          accept="image/*"
+                          style={{ display: "none" }}
+                          onChange={handleProfilePicUpload}
+                        />
+                      </div>
                       <img
                         src={`${BASE_URL}/${userProfile.userId.profilePicture}`}
                         alt="profilePicture"

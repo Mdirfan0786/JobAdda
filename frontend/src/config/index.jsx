440a9fb2385d41da1e751abc84904caf8ca9466e
Md Irfan <rn555666777@gmail.com>
2026-02-03 03:42:56 +0530

    backend url adde and ready for frontend deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:7870";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
